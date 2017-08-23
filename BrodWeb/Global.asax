00d7248785719fd70c3ff65f3d37d68dd497e27f
jedrzej.wiatrak <j.wiatrak@benefitsystems.pl>
2017-08-23 16:39:03 +0200

    initial comint

---
+++ BrodWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BrodWeb.MvcApplication" Language="C#" %>
---
+++ BrodWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BrodWeb.Startup))]
+namespace BrodWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
